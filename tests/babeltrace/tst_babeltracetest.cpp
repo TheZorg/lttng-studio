bdc663e8ffd605bfa06d01cf29712c3da41f9b2e
Fabien Reumont Locke <fabien.reumont-locke@polymtl.ca>
2014-08-27 12:05:32 -0400

    Testing parallel iteration
    Added test for parallel iteration of CTF traces.

--- tests/babeltrace/tst_babeltracetest.cpp
+++ tests/babeltrace/tst_babeltracetest.cpp
@@ -2,8 +2,11 @@
 #include <QtTest>
 #include <QDir>
 #include <QProcessEnvironment>
+#include <QThread>
+#include <QtConcurrent/QtConcurrentRun>
 
 #include <iostream>
+#include <cstdio>
 
 #include <babeltrace/babeltrace.h>
 #include <babeltrace/format.h>
@@ -25,6 +28,7 @@ private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
     void testEventCount();
+    void testMultiIter();
 private:
     QDir traceDir;
 };
@@ -43,6 +47,54 @@ void BabeltraceTest::initTestCase()
 
 void BabeltraceTest::cleanupTestCase()
 {
+}
+
+int countEvents(bt_ctf_iter *iter)
+{
+    struct bt_ctf_event *ctf_event;
+    int count = 0;
+
+
+    while((ctf_event = bt_ctf_iter_read_event(iter))) {
+        count++;
+        bt_iter_next(bt_ctf_get_iter(iter));
+    }
+
+    return count;
+}
+
+void BabeltraceTest::testMultiIter()
+{
+    bt_ctf_iter *iter1, *iter2;
+    int count1, count2;
+    count1 = count2 = 0;
+    struct bt_iter_pos begin_pos;
+
+    QString path = traceDir.absolutePath() + QDir::separator() + "succeed" +
+            QDir::separator() + "lttng-modules-2.0-pre5";
+
+    // open a trace
+    struct bt_context *ctx1 = bt_context_create();
+    int trace_id = bt_context_add_trace(ctx1, path.toStdString().c_str(), "ctf", NULL, NULL, NULL);
+    QVERIFY2(trace_id >= 0, "Failed: bt_context_add_trace");
+    begin_pos.type = BT_SEEK_BEGIN;
+    iter1 = bt_ctf_iter_create(ctx1, &begin_pos, NULL);
+
+    struct bt_context *ctx2 = bt_context_create();
+    trace_id = bt_context_add_trace(ctx2, path.toStdString().c_str(), "ctf", NULL, NULL, NULL);
+    QVERIFY2(trace_id >= 0, "Failed: bt_context_add_trace");
+    begin_pos.type = BT_SEEK_BEGIN;
+    iter2 = bt_ctf_iter_create(ctx2, &begin_pos, NULL);
+
+    QFuture<int> countFuture1 = QtConcurrent::run(countEvents, iter1);
+    QFuture<int> countFuture2 = QtConcurrent::run(countEvents, iter2);
+
+    count1 = countFuture1.result();
+    count2 = countFuture2.result();
+
+    bt_context_put(ctx1);
+    bt_context_put(ctx2);
+    QVERIFY2(count1 == count2, "Wrong event count");
 }
 
 void BabeltraceTest::testEventCount()
